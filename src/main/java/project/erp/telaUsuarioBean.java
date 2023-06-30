43306cfced413110276a2841dafaf0184f1fed0d
Fabricio C Santos <fsantos.veredinha@gmail.com>
2023-06-30 12:00:29 -0300

    AutoComplete
    Criação do autoComplete e listas de cidades para sugestão

--- src/main/java/project/erp/telaUsuarioBean.java
+++ src/main/java/project/erp/telaUsuarioBean.java
@@ -1,9 +1,13 @@
 package project.erp;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 @ManagedBean
 @ViewScoped
@@ -13,18 +17,50 @@ public class telaUsuarioBean implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	
+	private List<String> cidades = new ArrayList<String>();
+	
 	private String razaoSocial;
 	private int cnpj;
+	private String cidade;
 	
+	public telaUsuarioBean() {
+		cidades.add("Belo Horizonte");
+		cidades.add("Belém");
+		cidades.add("Brasília	");
+		cidades.add("Curitiba");
+		cidades.add("Fortaleza");
+		cidades.add("Gramado");
+		cidades.add("Manaus");
+		cidades.add("Fortaleza");
+		cidades.add("Ouro preto");
+		cidades.add("Recife");
+		cidades.add("Rio de Janeiro");
+		cidades.add("São Paulo");
+	}
+	
+	public List<String> sugerirCidades(String consulta) {
+		List<String> cidadesSugeridos = new ArrayList<String>();
+		
+		for (String cidade : this.cidades) {
+			if (cidade.toLowerCase().startsWith(consulta.toLowerCase())) {
+				cidadesSugeridos.add(cidade);
+			}
+		}
+		
+		return cidadesSugeridos;
+	}
 	
 	public void cadastrar() {
 		infoCadastro();
+		
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Cadastro Efetuado!"));
 	}
 	
 	public void infoCadastro() {
 		System.out.println("Razão Social: " + this.razaoSocial);
 		System.out.println("CPNJ: " + this.cnpj);
+		System.out.println("Cidade de contrato: " + this.cidade);
 	}
 	
 	/* Getters e Setters */
@@ -43,6 +79,14 @@ public class telaUsuarioBean implements Serializable {
 
 	public void setCnpj(int cnpj) {
 		this.cnpj = cnpj;
-	}	
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
 	
 }
