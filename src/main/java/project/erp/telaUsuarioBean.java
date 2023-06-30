3a2791a0bdc43bfaebe594300d21d4c868f8a747
Fabricio C Santos <fsantos.veredinha@gmail.com>
2023-06-30 11:03:23 -0300

    Razão Social, Botão Cadastrar
    Inicio da criação da tela, criação do campo "Razão Social", botão cadastrar e do campo CSS

--- src/main/java/project/erp/telaUsuarioBean.java
+++ src/main/java/project/erp/telaUsuarioBean.java
@@ -14,6 +14,26 @@ public class telaUsuarioBean implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	private String razaoSocial;
 	
+	
+	
+	public void cadastrar() {
+		infoCadastro();
+	}
+	
+	public void infoCadastro() {
+		System.out.println("Razão Social: " + this.razaoSocial);
+	}
+	
+	/* Getters e Setters */
+	
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}	
 	
 }
